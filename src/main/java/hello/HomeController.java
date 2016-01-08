0d3545f3e3654bfb4cfdfaf5ae532abd356f1a24
Adam Tokaj <kontakt@adamtokaj.pl>
2016-01-08 18:18:00 +0100

    lekka zmiana struktury dzialajaca startowa strona (wysietla sie strona logowania) balagan w HomeController

--- src/main/java/hello/HomeController.java
+++ src/main/java/hello/HomeController.java
@@ -6,8 +6,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 class HomeController {
 
-	@RequestMapping("/")
+	@RequestMapping(value = "/")
 	String index() {
 		return "index";
+	}
+
+	@RequestMapping(value = "/views/login.html")
+	String login() {
+		return "views/login";
+	}
+
+	@RequestMapping(value = "/views/panel.html")
+	String panel() {
+		return "views/panel";
+	}
+
+	@RequestMapping(value = "/views/main.html")
+	String main() {
+		return "views/main";
 	}
 }
